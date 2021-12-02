c3dfddb8a1f5204d3f811a2f44bac695ec0eb519
Kevin McDonald <kevin@simulmedia.com>
2021-12-02 15:40:52 -0700

    more comments

--- 2021/bash/scaffold.sh
+++ 2021/bash/scaffold.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# we have 25 days. we'll use an expansion to loop through each
 for i in {1..25}
 do
-  dir=$(printf "%02d" $i)
+  dir=$(printf "%02d" $i) # so they sort nicely, we'll make sure each is 2 digits
+
+  # if the directory doesn't exist, scaffold it out!
   if [ ! -d $dir ]
   then
     echo "Scaffolding day $i..."
     mkdir $dir
+
+    # we'll add a simple readme to each day with a link to the exercise.
+    # hopefully they don't change how they do it!
+    # oh, this is a "heredoc". we'll talk about those later...
     cat <<_EOF_ > $dir/README.md
 [Day $i](https://adventofcode.com/2021/day/$i)
 
